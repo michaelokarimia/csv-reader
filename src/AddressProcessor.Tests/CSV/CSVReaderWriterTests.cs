2482434f0ca9bc21cf293b7d483d66c9f3ddc4db
Michael Okarimia <git@michaelokarimia.com>
2017-05-20 16:21:14 +0100

    Adds test for Open method in CSVReaderWriter

--- src/AddressProcessor.Tests/CSV/CSVReaderWriterTests.cs
+++ src/AddressProcessor.Tests/CSV/CSVReaderWriterTests.cs
@@ -1,7 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using System.IO;
+using AddressProcessing.CSV;
 using NUnit.Framework;
 
 namespace Csv.Tests
@@ -9,5 +7,30 @@ namespace Csv.Tests
     [TestFixture]
     public class CSVReaderWriterTests
     {
+        private CSVReaderWriter _subject;
+        private string contacts_test_data_cvs;
+
+        [SetUp]
+        public void Setup()
+        {
+            contacts_test_data_cvs = @"test_data\contacts.csv";
+
+            _subject = new CSVReaderWriter();
+        }
+
+        [Test]
+        public void Opens_Valid_File_without_exception()
+        {
+
+            Assert.DoesNotThrow(() =>_subject.Open(contacts_test_data_cvs, CSVReaderWriter.Mode.Read));
+        }
+
+        [Test]
+        public void Throws_Exception_when_Opening_invalid_filepath()
+        {
+
+            Assert.Throws<FileNotFoundException>(() => _subject.Open(@"test_data\FileWhichDoesNotExist.dat", CSVReaderWriter.Mode.Read));
+        }
+
     }
 }
