5c5c3d713dc7f2994d3df616d472dafcf0c0d66a
Michael Okarimia <git@michaelokarimia.com>
2017-05-20 15:48:47 +0100

    Init commit + Annotated comments of CSVReadWriter
    Adds comments of the CSVReaderWriter class

---
+++ src/AddressProcessor/CSV/CSVReaderWriter For Annotation.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.IO;
+
+namespace AddressProcessing.CSV
+{
+    /*
+        1) List three to five key concerns with this implementation that you would discuss with the junior developer. 
+
+        Please leave the rest of this file as it is so we can discuss your concerns during the next stage of the interview process.
+        *) Class has two responsibilities, it should have one. Reading and Writing CSVs is different responsibility.
+        *) Class design allows methods to be called on it that could read or write to an uninitialised stream. Constructor of class ought to initialise the Stream readers & writers
+        *) Read method on line 60 advances the StreamReader, does nothing of use with paramters passed in, it's name does not reflect intent of method.  
+        *) Open method does two separate & unrelated tasks, it should have a single responsibility, but it has two. 
+           Effectively a boolean is passed in as a parameter and unrelated & different tasks are executed. There should be two separate methods instead.
+        *) Opens Streams without disposing them. All uses of streams should be wrapped in "using" statements so they are safely disposed
+        
+    */
+
+    public class CSVReaderWriterForAnnotation
+    {
+        private StreamReader _readerStream = null;
+        private StreamWriter _writerStream = null;
+
+        [Flags]
+        public enum Mode { Read = 1, Write = 2 };
+
+        public void Open(string fileName, Mode mode)
+        {
+            if (mode == Mode.Read)
+            {
+                _readerStream = File.OpenText(fileName);
+            }
+            else if (mode == Mode.Write)
+            {
+                FileInfo fileInfo = new FileInfo(fileName);
+                _writerStream = fileInfo.CreateText();
+            }
+            else
+            {
+                throw new Exception("Unknown file mode for " + fileName);
+            }
+        }
+
+        public void Write(params string[] columns)
+        {
+            string outPut = "";
+
+            for (int i = 0; i < columns.Length; i++)
+            {
+                outPut += columns[i];
+                if ((columns.Length - 1) != i)
+                {
+                    outPut += "\t";
+                }
+            }
+
+            WriteLine(outPut);
+        }
+
+        public bool Read(string column1, string column2)
+        {
+            const int FIRST_COLUMN = 0;
+            const int SECOND_COLUMN = 1;
+
+            string line;
+            string[] columns;
+
+            char[] separator = { '\t' };
+
+            line = ReadLine();
+            columns = line.Split(separator);
+
+            if (columns.Length == 0)
+            {
+                column1 = null;
+                column2 = null;
+
+                return false;
+            }
+            else
+            {
+                column1 = columns[FIRST_COLUMN];
+                column2 = columns[SECOND_COLUMN];
+
+                return true;
+            }
+        }
+
+        public bool Read(out string column1, out string column2)
+        {
+            const int FIRST_COLUMN = 0;
+            const int SECOND_COLUMN = 1;
+
+            string line;
+            string[] columns;
+
+            char[] separator = { '\t' };
+
+            line = ReadLine();
+
+            if (line == null)
+            {
+                column1 = null;
+                column2 = null;
+
+                return false;
+            }
+
+            columns = line.Split(separator);
+
+            if (columns.Length == 0)
+            {
+                column1 = null;
+                column2 = null;
+
+                return false;
+            } 
+            else
+            {
+                column1 = columns[FIRST_COLUMN];
+                column2 = columns[SECOND_COLUMN];
+
+                return true;
+            }
+        }
+
+        private void WriteLine(string line)
+        {
+            _writerStream.WriteLine(line);
+        }
+
+        private string ReadLine()
+        {
+            return _readerStream.ReadLine();
+        }
+
+        public void Close()
+        {
+            if (_writerStream != null)
+            {
+                _writerStream.Close();
+            }
+
+            if (_readerStream != null)
+            {
+                _readerStream.Close();
+            }
+        }
+    }
+}
